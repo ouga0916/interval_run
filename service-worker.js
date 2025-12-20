5359da42a1d459798f5adcd5542e55aa68362069
ouga0916 <og.h.m.a.ohana@gmail.com>
2025-12-20 12:51:30 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,22 @@
+const CACHE_NAME = "v2";
+const ASSETS = [
+  "./index.html",
+  "./result.html",
+  "./count.js",
+  "./result.js",
+  "./count.css",
+  "./result.css",
+  "./audio1.mp3",
+  "./audio2.mp3",
+  "./manifest.json",
+  "./clock_icon.png"
+];
+
 self.addEventListener("install", e => {
   e.waitUntil(
-    caches.open("v2").then(cache => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./result.html",
-        "./count.js",
-        "./result.js",
-        "./count.css",
-        "./result.css",
-        "./audio1.mp3",
-        "./audio2.mp3",
-        "./manifest.json",
-        "./clock_icon.png"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      // 1つでもファイルが見つからないと install に失敗するので注意
+      return cache.addAll(ASSETS);
     })
   );
 });
